382b95313dd6b975c9f7d8e9c0d0ec8314c26f39
mrkarthickdevil <83256566+mrkarthickdevil@users.noreply.github.com>
2021-05-04 15:48:06 +0530

    It's just fun game
    Just for fun

---
+++ karthick question and answer game.py
@@ -0,0 +1,11 @@
+print('welcome to question and answer game')
+
+name = input('What is your name?\n')
+print('Hello, %s!' % name)
+name = input('whitch tool you like for termux?\n')
+print('gud, %s!' % name)
+
+name = input('Which hacking you link?\n')
+print('nice, %s!' % name)
+
+print('you win')
